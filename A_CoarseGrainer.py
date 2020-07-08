0c7ae210d8bd8622c377b37511e85e0172a0b602
Forrest Bicker <bickerforrest@gmail.com>
2020-07-08 17:59:32 -0400

    update mapping_dict symbol name

--- A_CoarseGrainer.py
+++ A_CoarseGrainer.py
@@ -61,12 +61,11 @@ for resname in residue_list:  # loops tru each residue to be coarse grained
     resids = resname_atoms.residues.resids
     for resid in resids:  # loops thu each matching residue id
         try:
-            segments = amino_acid_dict[resname_root].keys()
+            segments = mapping_dict[resname_root].keys()
         except KeyError:
             print('{resname_root} was not found in amino_acid_dict. Please add its parameters to the dictionary. (See README section A3. for help)')
             raise
         for segment in segments:  # loops thru each segment of each residue
-            name_params = ' '.join(amino_acid_dict[resname_root][segment])
             name_params = ' '.join(mapping_dict[resname_root][segment])
             params = f'resname {resname} and resid {resid} and (name {name_params})'
             # selects all atoms in a given residue segment
