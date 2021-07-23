ae7505cea589598b7d0b41ab66a0bbffc0079ec9
Forrest Bicker <bickerforrest@gmail.com>
2021-07-23 19:44:11 -0400

    create dash app skeleton with static layout

--- src/app.py
+++ src/app.py
@@ -32,3 +32,40 @@ def wrap(component):
         className='wrapper'
     )
 
+measurement_number = 0
+potentials = {}
+
+def startup_manual_refining(measurement_dict, u):
+
+
+    app.layout = html.Div(
+        children=[
+            wrap([
+                html.H1('Curve Fitting Tool'),
+                html.P('Slide the slider to adjust the size of the bins that the raw measurements are clustered into. Select a region of interest on the graph to adjust the subset of data to which the curve is fitted. When you are happy with the results, click NEXT to view the next graph or EXPORT to write your parametization data to a file.'),
+                html.H2('Adjust Bin Size'),
+                dcc.Slider(
+                    id='bin-slider',
+                    min=0.001,
+                    max=1,
+                    step=0.001,
+                    value=0.01,
+                ),
+                html.H2(id='page-number'),
+                html.Button(id='back-button', n_clicks=0, children='Back'),
+                html.Button(id='next-button', n_clicks=0, children='Next'),
+                html.Button(id='export-button-1', n_clicks=0, children='Export Params'),
+                Download(id='export-download-1'),
+                html.Button(id='export-button-2', n_clicks=0, children='Export .top'),
+                Download(id='export-download-2'),
+                html.H2(id='results'),
+                dcc.Graph(
+                    id='fitting-graph',
+                ),
+            ])
+        ]
+    )
+
+
+
+
